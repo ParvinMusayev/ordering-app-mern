6993bb0c4bdb509efcb9c1a6da95bb58e573b713
ParvinMusayev <parvinmusayev1@gmail.com>
2024-02-20 00:46:21 +0400

    chore: update home page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,3 +1,8 @@
+
+//images
+import landingImage from "../assets/landing.png";
+import appDownloadImage from "../assets/appDownload.png";
+
 const HomePage = () => {
   return (
     <div className="flex flex-col gap-12">
@@ -6,6 +11,21 @@ const HomePage = () => {
           Tuck into a takway today
         </h1>
         <span className="text-xl ">Food is just a click away!</span>
+      </div>
+
+      <div className="grid md:grid-cols-2 gap-5">
+        <img src={landingImage} alt="landingimage" />
+
+        <div className="flex flex-col items-center justify-center gap-4 text-center">
+          <span className="font-bold text-3xl tracking-tighter">
+            Order takeaway even faster!
+          </span>
+          <span>
+            Download the MernEats App for faster ordering and personalised
+            recommendations
+          </span>
+          <img src={appDownloadImage} alt="download" />
+        </div>
       </div>
     </div>
   );
