56f4fa0bd49060c2654a11f9afebfea16d59fd63
ParvinMusayev <parvinmusayev1@gmail.com>
2024-02-20 00:31:45 +0400

    feat: Add home page

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+const HomePage = () => {
+  return (
+    <div className="flex flex-col gap-12">
+      <div className="flex flex-col gap-5 text-center -mt-16 bg-white rounded-lg shadow-md py-8">
+        <h1 className="text-5xl font-bold tracking-tight text-orange-600">
+          Tuck into a takway today
+        </h1>
+        <span className="text-xl ">Food is just a click away!</span>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
