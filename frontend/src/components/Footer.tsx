d96f996a90dbf256ae6a8c4f5c42626a819927c7
ParvinMusayev <parvinmusayev1@gmail.com>
2024-02-20 00:52:39 +0400

    feat: Add footer

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <div className="bg-orange-500 py-10">
+      <div className="container flex flex-col md:flex-row justify-between items-center mx-auto">
+        <span className="text-3xl text-white font-bold tracking-tight">
+          MessiEats.com
+        </span>
+        <span className="text-white font-bold tracking-tight flex gap-4">
+          <span>Privacy Policy</span>
+          <span>Terms of Service</span>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
