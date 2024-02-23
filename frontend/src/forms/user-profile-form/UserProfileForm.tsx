d56b48742a71741a82cf89b461217465ce53293a
ParvinMusayev <parvinmusayev1@gmail.com>
2024-02-23 15:04:37 +0400

    feat: Add user profile form

---
+++ frontend/src/forms/user-profile-form/UserProfileForm.tsx
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+import { User } from "@/types";
+
+const formSchema = z.object({
+  email: z.string().optional(),
+  name: z.string().min(1, "name is required"),
+  addressLine1: z.string().min(1, "Address Line 1 is required"),
+  city: z.string().min(1, "City is required"),
+  country: z.string().min(1, "Country is required"),
+});
+
+export type UserFormData = z.infer<typeof formSchema>;
+
+type Props = {
+  currentUser: User;
+  onSave: (userProfileData: UserFormData) => void;
+  isLoading: boolean;
+  title?: string;
+  buttonText?: string;
+};
