6523ad1e8b096299963e743bbe46840d69181d36
ParvinMusayev <parvinmusayev1@gmail.com>
2024-02-23 18:25:14 +0400

    chore: update profile page

--- frontend/src/pages/UserProfilePage.tsx
+++ frontend/src/pages/UserProfilePage.tsx
@@ -1,13 +1,10 @@
+import { useUpdateMyUser } from "@/api/MyUserApi";
 import UserProfileForm from "@/forms/user-profile-form/UserProfileForm";
 
 const UserProfilePage = () => {
- 
+  const { updateUser, isLoading: isUpdateLoading } = useUpdateMyUser();
 
-  return (
-    <UserProfileForm
-      
-    />
-  );
+  return <UserProfileForm onSave={updateUser} isLoading={isUpdateLoading} />;
 };
 
 export default UserProfilePage;
