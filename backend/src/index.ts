5b7474d7a849d9918e980875a767bedd1128123e
ParvinMusayev <parvinmusayev1@gmail.com>
2024-02-24 14:52:21 +0400

    test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,23 @@
-import express, { Request, Response } from "express";
-import "dotenv/config";
-import cors from "cors";
-import mongoose from "mongoose";
-import myUserRoute from './routes/MyUserRoute'
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const myUserRoute = require("./routes/MyUserRoute");
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(()=> console.log("Connected to database"))
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING).then(() => console.log("Connected to database"));
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/health", async (req: Request, res: Response) => {
+app.get("/health", async (req, res) => {
   res.send({ message: "health OK!" });
 });
 
-app.use("/api/my/user", myUserRoute)
+app.use("/api/my/user", myUserRoute);
 
 app.listen(7000, () => {
   console.log("server started on localhost:7000");
