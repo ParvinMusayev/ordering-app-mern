798bf3d40ad563af068e29c251f2ba8ea4fd4fa1
ParvinMusayev <parvinmusayev1@gmail.com>
2024-02-25 20:59:47 +0400

    feat: Add cuisines section

--- frontend/src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
+++ frontend/src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -3,6 +3,8 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Form } from "@/components/ui/form";
 import DetailsSection from "./DetailsSection";
+import { Separator } from "@/components/ui/separator";
+import CuisinesSection from "./CuisinesSection";
 
 const formSchema = z.object({
   restaurantName: z.string({
@@ -59,6 +61,8 @@ const ManageRestaurantForm = ({ onSave, isLoading }: Props) => {
         className="space-y-8 bg-gray-50 p-10 rounded-lg"
       >
            <DetailsSection />
+           <Separator />
+           <CuisinesSection />
       </form>
     </Form>
   );
