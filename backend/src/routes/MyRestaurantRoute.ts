b80dfab84a638f12a46474ab66172a20fc788e79
ParvinMusayev <parvinmusayev1@gmail.com>
2024-02-25 16:54:32 +0400

    chore: update validation

--- backend/src/routes/MyRestaurantRoute.ts
+++ backend/src/routes/MyRestaurantRoute.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import multer from "multer";
 import MyRestaurantController from "../controllers/MyRestaurantController";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyRestaurantRequest } from "../middleware/validation";
 
 const router = express.Router();
 const storage = multer.memoryStorage();
@@ -15,6 +17,9 @@ const upload = multer({
 router.post(
   "/",
   upload.single("imageFile"),
+  validateMyRestaurantRequest,
+  jwtCheck,
+  jwtParse,
   MyRestaurantController.createMyRestaurant
 );
 
