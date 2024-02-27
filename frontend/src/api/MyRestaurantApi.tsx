10d7723d86cd464d110823ec76aad67d0a016312
ParvinMusayev <parvinmusayev1@gmail.com>
2024-02-27 18:25:05 +0400

    chore: update restaurant api

--- frontend/src/api/MyRestaurantApi.tsx
+++ frontend/src/api/MyRestaurantApi.tsx
@@ -1,3 +1,4 @@
+import { Restaurant } from "@/types";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useMutation } from "react-query";
 
@@ -8,7 +9,9 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export const useCreateMyRestaurant = () => {
   const { getAccessTokenSilently } = useAuth0();
 
-  const createMyRestaurantRequest = async (restaurantFormData: FormData) => {
+  const createMyRestaurantRequest = async (
+    restaurantFormData: FormData
+  ): Promise<Restaurant> => {
     const accessToken = await getAccessTokenSilently();
 
     const response = await fetch(`${API_BASE_URL}/api/my/restaurant`, {
