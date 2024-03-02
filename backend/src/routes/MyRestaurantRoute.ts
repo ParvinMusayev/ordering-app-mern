b1c1472c821a445ee492e731b0d272e3fe0e19d6
ParvinMusayev <parvinmusayev1@gmail.com>
2024-03-02 17:57:24 +0400

    chore: update restaurant controller

--- backend/src/routes/MyRestaurantRoute.ts
+++ backend/src/routes/MyRestaurantRoute.ts
@@ -25,4 +25,15 @@ router.post(
   MyRestaurantController.createMyRestaurant
 );
 
+router.put(
+  "/",
+  upload.single("imageFile"),
+  validateMyRestaurantRequest,
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.updateMyRestaurant
+);
+
+
+
 export default router;
