5cb1d58924c6a5e2b6bc37f8f55497b98d82492f
ParvinMusayev <parvinmusayev1@gmail.com>
2024-03-03 18:12:52 +0400

    feat: Add search page

---
+++ frontend/src/pages/SearchPage.tsx
@@ -0,0 +1,16 @@
+import { useParams } from "react-router-dom";
+
+export type SearchState = {
+  searchQuery: string;
+  page: number;
+  selectedCuisines: string[];
+  sortOption: string;
+};
+
+const SearchPage = () => {
+  const { city } = useParams();
+
+  return <span>User searched for {city}</span>;
+};
+
+export default SearchPage;
