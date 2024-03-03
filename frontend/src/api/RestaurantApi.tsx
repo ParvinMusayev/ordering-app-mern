abedb4feb31005309077309b2f49286b583b296c
ParvinMusayev <parvinmusayev1@gmail.com>
2024-03-03 18:20:47 +0400

    feat: Add restaurant api

---
+++ frontend/src/api/RestaurantApi.tsx
@@ -0,0 +1,27 @@
+import { useQuery } from "react-query";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const useSearchRestaurants = (city?: string) => {
+  const createSearchRequest = async () => {
+    const response = await fetch(
+      `${API_BASE_URL}/api/restaurant/search/${city}`
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to get restaurant");
+    }
+
+    return response.json();
+  };
+
+  const { data: results, isLoading } = useQuery(
+    ["searchRestaurants"],
+    createSearchRequest
+  );
+
+  return {
+    results,
+    isLoading,
+  };
+};
