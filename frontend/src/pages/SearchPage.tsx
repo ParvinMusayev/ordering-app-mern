e1c80775204c2ba781a64823790667aec18d8932
ParvinMusayev <parvinmusayev1@gmail.com>
2024-03-06 18:53:44 +0400

    chore: update search page

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,18 +1,44 @@
 import { useSearchRestaurants } from "@/api/RestaurantApi";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
 import SearchResultCard from "@/components/SearchResultCard";
 import SearchResultInfo from "@/components/SearchResultInfo";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 
 export type SearchState = {
   searchQuery: string;
-  page: number;
-  selectedCuisines: string[];
-  sortOption: string;
+  // page: number;
+  // selectedCuisines: string[];
+  // sortOption: string;
 };
 
 const SearchPage = () => {
   const { city } = useParams();
+
+  const [searchState, setSearchState] = useState<SearchState>({
+    searchQuery: "",
+    // page: 1,
+    // selectedCuisines: [],
+    // sortOption: "bestMatch",
+  });
+
   const { results, isLoading } = useSearchRestaurants(city);
+
+  const setSearchQuery = (searchFormData: SearchForm) => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      searchQuery: searchFormData.searchQuery,
+      page: 1,
+    }));
+  };
+
+  const resetSearch = () => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      searchQuery: "",
+      page: 1,
+    }));
+  };
 
   if (isLoading) {
     <span>Loading ...</span>;
@@ -26,6 +52,12 @@ const SearchPage = () => {
     <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
       <div id="cuisines-list">insert cuisines here</div>
       <div id="main-content" className="flex flex-col gap-5">
+        <SearchBar
+          searchQuery={searchState.searchQuery}
+          onSubmit={setSearchQuery}
+          placeHolder="Search by Cuisine or Restaurant Name"
+          onReset={resetSearch}
+        />
         <SearchResultInfo total={results.pagination.total} city={city} />
         {results.data.map((restaurant) => (
           <SearchResultCard restaurant={restaurant} />
