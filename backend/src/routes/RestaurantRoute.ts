2ca960127b05420d0da9b6756cfa581e722dfa47
ParvinMusayev <parvinmusayev1@gmail.com>
2024-03-13 21:04:47 +0400

    chore: update restaurant controller

--- backend/src/routes/RestaurantRoute.ts
+++ backend/src/routes/RestaurantRoute.ts
@@ -5,6 +5,16 @@ import RestaurantController from "../controllers/RestaurantController";
 const router = express.Router();
 
 router.get(
+  "/:restaurantId",
+  param("restaurantId")
+    .isString()
+    .trim()
+    .notEmpty()
+    .withMessage("RestaurantId paramenter must be a valid string"),
+  RestaurantController.getRestaurant
+);
+
+router.get(
   "/search/:city",
   param("city")
     .isString()
@@ -13,5 +23,3 @@ router.get(
     .withMessage("City paramenter must be a valid string"),
   RestaurantController.searchRestaurant
 );
-
-export default router;
