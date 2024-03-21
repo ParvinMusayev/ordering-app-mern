076edaa2539368d67d5a84386b9d279972687dbd
ParvinMusayev <parvinmusayev1@gmail.com>
2024-03-22 00:41:29 +0400

    feat: Add badge

--- frontend/src/components/OrderSummary.tsx
+++ frontend/src/components/OrderSummary.tsx
@@ -1,7 +1,7 @@
 import { CartItem } from "@/pages/DetailPage";
 import { Restaurant } from "@/types";
 import { CardContent, CardHeader, CardTitle } from "./ui/card";
-
+import { Badge } from "./ui/badge";
 import { Separator } from "./ui/separator";
 import { Trash } from "lucide-react";
 
@@ -34,7 +34,12 @@ const OrderSummary = ({ restaurant, cartItems, removeFromCart }: Props) => {
       <CardContent className="flex flex-col gap-5">
         {cartItems.map((item) => (
           <div className="flex justify-between">
-            <span>{item.name}</span>
+            <span>
+              <Badge variant="outline" className="mr-2">
+                {item.quantity}
+              </Badge>
+              {item.name}
+            </span>
             <span className="flex items-center gap-1">
               <Trash
                 className="cursor-pointer"
